a8a58f8d0c48950cdc6058e65ffac5935aab3d83
Anshul Singh <anshulsingh024@yahoo.com>
2023-04-15 15:20:55 +0530

    made login system

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#include<string>
+#include<map>
 using namespace std;
 
 int main()
 {
-    cout<<"Hello World";
+    // creating the dictonary
+    map<string, string>dict = { {"your_username", "password-auth1"} }; //maps the username and password to randomly generated sequels
+    string username, password;
+    cout<<"Enter username: "<<endl;
+    cin>>username;
+    cout<<"Enter password: "<<endl;
+    cin>>password;
+    // Checks if provided username and password matches with the one in dictonary
+    if(dict.find(username) != dict.end() && dict[username] == password)  // if username doesn't returns the end string(i.e. not found), it is valid, and if password matches, login granted
+    {
+        cout<<"Logged in Successfully!";
+    } 
+    else {
+        cout<<"Invalid Credentials";
+        cout<<"Please check your username and password";
+    }
     return 0;
 }
